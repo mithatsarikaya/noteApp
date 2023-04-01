73f483b9f8e8129aad46a325a1e786b9d1f642da
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-04-01 12:41:20 +0300

    user can understand which 'note' he/she selected

--- noteAppVanillaJs/app.js
+++ noteAppVanillaJs/app.js
@@ -1,1 +1,15 @@
+const notes = document.querySelectorAll(".note");
+// console.log(selectedNote.classList.add("selected--note"));
 
+notes.forEach((note) => {
+  note.addEventListener("click", (e) => {
+    let selectedNote = e.target;
+
+    //add 'selectedNote' style to the div, if not selected then remove it
+    notes.forEach((n) => {
+      Array.from(n.classList).includes(selectedNote.classList[1])
+        ? n.classList.add("selected--note")
+        : n.classList.remove("selected--note");
+    });
+  });
+});
